18d5cf680c27d7a8f03540b6bb1e23f8aa1d000f
Adam Munns <adam@whetinteractive.com>
2023-03-06 11:07:00 -0800

    fix: bit more swr and svg refining

--- src/ui/components/Avatar/index.tsx
+++ src/ui/components/Avatar/index.tsx
@@ -1,36 +1,33 @@
 import { AvatarResolver } from '@ensdomains/ens-avatar'
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
-import React, { useMemo, useState } from 'react'
+import React from 'react'
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
-
+import useSWR from 'swr'
 const provider = new StaticJsonRpcProvider('https://ethereum.publicnode.com')
 
 const Avatar = ({
   authorAddress,
   size = 48
 }: {
-  authorAddress: any
+  authorAddress: string
   size?: number
 }) => {
   const diameter = size ? size : 48
-  const [avatar, setAvatar] = useState('')
+  const getAvatar = async () => {
+    const avt = new AvatarResolver(provider)
+    const avatarURI = await avt.getAvatar(authorAddress, {})
+    return avatarURI
+  }
 
-  useMemo(() => {
-    if (!authorAddress) return
-    const getAvatar = async () => {
-      const avt = new AvatarResolver(provider)
-      const avatarURI = await avt.getAvatar(authorAddress, {
-        /* jsdomWindow: jsdom (on nodejs) */
-      })
-      if (avatarURI) {
-        setAvatar(avatarURI)
-      }
-    }
-    getAvatar()
-  }, [authorAddress])
+  const swrData = useSWR('getAvatar', getAvatar)
+  const { data: avatar, isLoading } = swrData
 
-  return avatar ? (
-    <img src={avatar} className="w-height h-full" alt="avatarENS" />
+  return avatar && !isLoading ? (
+    <img
+      src={avatar}
+      className="w-height h-full rounded-full"
+      alt="avatarENS"
+    />
   ) : (
     <Jazzicon diameter={diameter} seed={jsNumberForAddress(authorAddress)} />
   )
--- src/ui/components/Loader/index.tsx
+++
@@ -1,6 +0,0 @@
-import './index.css'
-import React from 'react'
-
-const Loader = () => <div className="loader-component" />
-
-export default Loader
